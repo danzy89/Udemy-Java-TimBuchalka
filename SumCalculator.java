8106e082bc7d29c5caaad59d2e50cab6f8f831e3
danzy89 <danzzy@gmail.com>
2022-04-30 10:58:39 +0500

    Add files via upload

---
+++ SumCalculator.java
@@ -0,0 +1,35 @@
+public class SumCalculator {
+
+    public static class SimpleCalculator {
+        private double firstNumber;
+        private double secondNumber;
+
+        public void setFirstNumber (double firstNumber) {
+            this.firstNumber = firstNumber;
+        }
+        public double getFirstNumber() {
+            return this.firstNumber;
+        }
+        public void setSecondNumber (double secondNumber) {
+            this.secondNumber = secondNumber;
+        }
+        public double getSecondNumber() {
+            return this.secondNumber;
+        }
+        public double getAdditionResult() {
+            return firstNumber + secondNumber;
+        }
+        public double getSubtractionResult() {
+            return firstNumber - secondNumber;
+        }
+        public double getMultiplicationResult() {
+            return firstNumber * secondNumber;
+        }
+        public double getDivisionResult() {
+            if (secondNumber == 0) {
+                return 0;
+            }
+            return firstNumber / secondNumber;
+        }
+    }
+}
